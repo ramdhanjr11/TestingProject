f969fd6faf09d185396f6bab25b9ad59f404cec2
yossyhds <yossyhds>
2021-05-16 18:19:58 +0700

    add variable sisyBinding

--- app/src/main/java/com/muramsyah/jetpackpro/testingproject/SisyActivity.kt
+++ app/src/main/java/com/muramsyah/jetpackpro/testingproject/SisyActivity.kt
@@ -2,10 +2,17 @@ package com.muramsyah.jetpackpro.testingproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.muramsyah.jetpackpro.testingproject.databinding.ActivitySisyBinding
 
 class SisyActivity : AppCompatActivity() {
+
+    private lateinit var sisyBinding: ActivitySisyBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sisy)
+        sisyBinding = ActivitySisyBinding.inflate(layoutInflater)
+        setContentView(sisyBinding.root)
+
+//        sisytralala
     }
 }
