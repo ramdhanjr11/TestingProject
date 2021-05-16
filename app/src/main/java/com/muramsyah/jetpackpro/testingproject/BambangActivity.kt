ee45a3c4996796c2e83bf3489696cbf0cea6536e
ramdhanjr11 <ramdhanjr690@gmail.com>
2021-05-16 18:38:59 +0700

    add binding in BambangActivity.kt

--- app/src/main/java/com/muramsyah/jetpackpro/testingproject/BambangActivity.kt
+++ app/src/main/java/com/muramsyah/jetpackpro/testingproject/BambangActivity.kt
@@ -2,8 +2,11 @@ package com.muramsyah.jetpackpro.testingproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.muramsyah.jetpackpro.testingproject.databinding.ActivityBambangBinding
 
 class BambangActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityBambangBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bambang)
