13c85d05b27803790b004a3f839400e269896d5d
yossyhds <yossyhds>
2021-05-16 17:51:34 +0700

    add SisyActivity.kt

---
+++ app/src/main/java/com/muramsyah/jetpackpro/testingproject/SisyActivity.kt
@@ -0,0 +1,11 @@
+package com.muramsyah.jetpackpro.testingproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SisyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sisy)
+    }
+}
